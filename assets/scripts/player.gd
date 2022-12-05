7e4f5a548b41de090c722594aeae72381c0d5dfe
GTNT-Github <graemeiburtis@gmail.com>
2022-12-05 16:27:29 -0500

    5.12.22

---
+++ assets/scripts/player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+export var speed = 250
+var faceLeft: bool = true
+
+
+func _process(_delta: float) -> void:
+	var velocity: Vector2 = getInput()
+	move_and_slide(velocity*speed)
+
+func getInput() -> Vector2:
+	var animatedSprite = $Sprite
+	var velocity: Vector2 = Vector2.ZERO
+	var animation: String = "idle"
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		animation = "walk"
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		animation = "walk"
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		animation = "walk"
+		faceLeft = false
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		animation = "walk"
+		faceLeft = true
+		
+	if animation != animatedSprite.animation:
+		animatedSprite.play(animation)
+		animatedSprite.flip_h = faceLeft
+	return velocity
