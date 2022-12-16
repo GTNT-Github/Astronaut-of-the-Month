f7fdec12cee2faba70ae4da34892c1522538553c
GTNT-Github <graemeiburtis@gmail.com>
2022-12-16 11:15:15 -0500

    16.12.22 Fix 2
    - Port Fowarding
    - Github Pages

--- assets/scripts/server.gd
+++ assets/scripts/server.gd
@@ -4,10 +4,24 @@ var multiplayerPeer = ENetMultiplayerPeer.new()
 
 const PORT = 9999
 const ADDRESS = "localhost"
-
+var upnp
 func start_server():
 	$UI/Host.visible = false
 	$UI/Join.visible = false
+	upnp = UPNP.new()
+	var discoverResult = upnp.discover()
+	
+	if discoverResult == UPNP.UPNP_RESULT_SUCCESS:
+		if upnp.get_gateway() and upnp.get_gateway().is_valid_gateway():
+			var mapResultTcp = upnp.add_port_mapping(9999,0,"godotUdp","TCP",0)
+			var mapResultUdp = upnp.add_port_mapping(9999,0,"godotUdp","UDP",0)
+			if not mapResultTcp == UPNP.UPNP_RESULT_SUCCESS:
+				upnp.add_port_mapping(9999,9999,"","TCP")
+			if not mapResultUdp == UPNP.UPNP_RESULT_SUCCESS:
+				upnp.add_port_mapping(9999,9999,"","UDP")
+	
+	var externalIP = upnp.query_external_address()
+	
 	multiplayerPeer.create_server(PORT)
 	multiplayer.multiplayer_peer = multiplayerPeer
 	multiplayerPeer.peer_connected.connect(func(id): addPlayerChar(id))
@@ -29,3 +43,8 @@ func addPlayerChar(id=1):
 
 func _on_multiplayer_spawner_spawned(node):
 	print(1)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST && UPNP:
+		upnp.delete_port_mapping(9999,"UDP")
+		upnp.delete_port_mapping(9999,"TCP")
