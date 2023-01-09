d2c58a575901f536c59e2dacdbd8f6f5dc448e15
GTNT-Github <graemeiburtis@gmail.com>
2023-01-09 15:25:51 -0500

    Server - Waiting Room

--- Server/server/server.gd
+++ Server/server/server.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 var network = NetworkedMultiplayerENet.new()
 var port = 3234
 var max_players = 4
@@ -13,18 +12,18 @@ func _ready():
 func start_server():
 	network.create_server(port, max_players)
 	get_tree().set_network_peer(network)
-	network.connect("peer_connected",self,"_player_connected")
-	network.connect("peer_disconnected",self,"_player_disconnected")
+	network.connect("peer_connected", self, "_player_connected")
+	network.connect("peer_disconnected", self, "_player_disconnected")
 	
 	print("Server Started")
 	
 func _player_connected(player_id):
-	print("Player: "+str(player_id)+" Connected")
-
+	print("Player: " + str(player_id) + " Connected")
+	
 func _player_disconnected(player_id):
-	print("Player: "+str(player_id)+" Disconnected")
-
-remote func send_player_info(id,player_data):
+	print("Player: " + str(player_id) + " Disconnected")
+	
+remote func send_player_info(id, player_data):
 	players[id] = player_data
 	rset("players", players)
 	rpc("update_waiting_room")
