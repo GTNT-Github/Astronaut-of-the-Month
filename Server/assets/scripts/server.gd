436f50d8dbac11d38ea34bedbfcc4ef22c23a8d6
GTNT-Github <graemeiburtis@gmail.com>
2023-01-09 16:12:28 -0500

    Server - World Fixed

--- Server/assets/scripts/server.gd
+++ Server/assets/scripts/server.gd
@@ -6,6 +6,7 @@ var max_players = 4
 
 var players = {}
 var ready_players = 0
+
 func _ready():
 	start_server()
 	
@@ -16,23 +17,21 @@ func start_server():
 	network.connect("peer_disconnected", self, "_player_disconnected")
 	
 	print("Server Started")
-
+	
 func _player_connected(player_id):
 	print("Player: " + str(player_id) + " Connected")
-
+	
 func _player_disconnected(player_id):
 	print("Player: " + str(player_id) + " Disconnected")
-
+	
 remote func send_player_info(id, player_data):
 	players[id] = player_data
 	rset("players", players)
 	rpc("update_waiting_room")
-
-
+	
 remote func load_world():
 	ready_players += 1
 	if players.size() > 1 and ready_players >= players.size():
 		rpc("start_game")
 		var world = preload("res://assets/scenes/World.tscn").instance()
 		get_tree().get_root().add_child(world)
-
